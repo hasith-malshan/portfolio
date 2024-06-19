b06e35a507006ebc74fab7398954a7e8cd0418b6
hmr53 <hasher22542@gmail.com>
2024-06-19 19:00:22 +0530

    Image Urls changed in Portfolio.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,8 +8,7 @@ const items = [
     id: 1,
     title: 'Self Projects',
     link: '/projects/self',
-    image:
-      'https://images.pexels.com/photos/23533610/pexels-photo-23533610/free-photo-of-the-tall-dark-and-handsome-oes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: '/project_details/selfProjects.jpeg',
     description:
       'These self-taught projects reflect my unwavering passion for web development and programming. I’ve delved into various facets, including mastering MVC architecture, building microservices, crafting RESTful APIs, and exploring modern front-end frameworks. My commitment to continuous learning drives me to seek out new challenges and expand my skill set. With each project, I embrace the opportunity to learn, create, and contribute to the ever-evolving world of technology.',
   },
@@ -17,8 +16,7 @@ const items = [
     id: 2,
     title: 'University Projects',
     link: '/projects/university',
-    image:
-      'https://images.pexels.com/photos/23533610/pexels-photo-23533610/free-photo-of-the-tall-dark-and-handsome-oes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: '/project_details/university_projects.jpg',
     description:
       'University projects play a crucial role in enhancing practical skills by allowing us to apply theoretical knowledge. During my studies, I gained a solid foundation in programming languages such as Java, JavaScript, and PHP, along with experience working with databases like MySQL. These projects have not only honed my technical abilities but also fostered problem-solving skills and creativity.',
   },
@@ -26,8 +24,7 @@ const items = [
     id: 3,
     title: 'Client Projects',
     link: '/projects/client',
-    image:
-      'https://images.pexels.com/photos/23533610/pexels-photo-23533610/free-photo-of-the-tall-dark-and-handsome-oes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: '/project_details/client_projects.jpg',
     description:
       'Working with clients provided a unique experience compared to university projects. While it was occasionally challenging, it significantly improved my communication skills, collaboration abilities, and other soft skills. Ultimately, I’m pleased that I could deliver the product they required.',
   },
@@ -44,7 +41,6 @@ const Single = ({ item }) => {
           <motion.div className="textContainer">
             <h2>{item.title}</h2>
             <p>{item.description}</p>
-            {console.log(item.link)}
             <Link to={item.link}>
               <button>Check Projects</button>
             </Link>
